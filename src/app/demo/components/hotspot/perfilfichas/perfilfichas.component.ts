7b99a553d5a9ff4c03b7dc30e11595ea8c11721a
Oscar Hernandez Martinez <oskr9922@gmail.com>
2022-11-08 20:10:46 -0600

    agregando el metodo POST para agergar nuevo perfil, funcionando con datos estaticos

--- src/app/demo/components/hotspot/perfilfichas/perfilfichas.component.ts
+++ src/app/demo/components/hotspot/perfilfichas/perfilfichas.component.ts
@@ -4,92 +4,111 @@ import { Table } from 'primeng/table';
 import { PerfilfichasService } from 'src/app/demo/service/perfilfichas.service';
 
 interface TipoFicha {
-    nombre: string,
-    value: string
+    nombre: string;
+    value: string;
     status: boolean;
 }
 
 @Component({
-  selector: 'app-perfilfichas',
-  templateUrl: './perfilfichas.component.html',
+    selector: 'app-perfilfichas',
+    templateUrl: './perfilfichas.component.html',
 })
-
-
 export class PerfilfichasComponent implements OnInit {
-//ARREGLO DONDE SE OBTIENEN LAS PERFILES DESDE EL FRONT
+    //ARREGLO DONDE SE OBTIENEN LAS PERFILES DESDE EL FRONT
 
-//variables prueba
-listplanesfichas: any[] = [];
-planes: any;
+    //variables prueba
+    listplanesfichas: any[] = [];
+    planes: any;
 
-//VARIABLE DE LOS VALORES DEL MODAL
-//false para que el modal no se abre cuando carga la pagina
-productDialog: boolean = false;
-submitted: boolean = false;
+    //VARIABLE DE LOS VALORES DEL MODAL
+    //false para que el modal no se abre cuando carga la pagina
+    perfilModalDialog: boolean = false;
+    submitted: boolean = false;
 
-//VARIABLES DE VALOR POR DEFAULT TIEMPO LIMITE DE FICHAS
-defaulttimpolimiteDia: string = "0";
-defaulttimpolimiteHora: string = "0";
-defaulttimpolimiteMin: string = "0";
+    //VARIABLES DE VALOR POR DEFAULT TIEMPO LIMITE DE FICHAS
+    defaulttimpolimiteDia: string = '0';
+    defaulttimpolimiteHora: string = '0';
+    defaulttimpolimiteMin: string = '0';
 
-//VARIABLES DE VALOR POR DEFAULT EXOIRACION DE FICHAAS
-defaultExpiracionDia: string = "0";
-defaultExpiracionHora: string = "0";
-defaultExpiracionMin: string = "0";
+    //VARIABLES DE VALOR POR DEFAULT EXOIRACION DE FICHAAS
+    defaultExpiracionDia: string = '0';
+    defaultExpiracionHora: string = '0';
+    defaultExpiracionMin: string = '0';
 
-//VARIABLES DE SELECCION DE TIPO DE FICHAS
-selectTipoFicha : TipoFicha [];
-fichaSelected? : TipoFicha;
-//VARIABLES DE VELOCIDAD DE TIPO DE DICHAS MB/KB
-selectVelocidadsubida : TipoFicha [];
-velocidadSelectedsubida? : TipoFicha;
-selectVelocidadbajada : TipoFicha [];
-velocidadSelectedbajada? : TipoFicha;
+    //VARIABLES DE SELECCION DE TIPO DE FICHAS
+    selectTipoFicha: TipoFicha[];
+    fichaSelected?: TipoFicha;
+    //VARIABLES DE VELOCIDAD DE TIPO DE DICHAS MB/KB
+    selectVelocidadsubida: TipoFicha[];
+    velocidadSelectedsubida?: TipoFicha;
+    selectVelocidadbajada: TipoFicha[];
+    velocidadSelectedbajada?: TipoFicha;
 
+    constructor(private _perfilfichas: PerfilfichasService) {
+        this.selectTipoFicha = [
+            { nombre: 'Corrido', value: 'CORRIDO', status: true },
+            { nombre: 'Pausado', value: 'PAUSADO', status: true },
+            { nombre: 'Megas', value: 'MEGAS', status: true },
+        ];
+        this.selectVelocidadsubida = [
+            { nombre: 'Kbps', value: 'K', status: true },
+            { nombre: 'Mbps', value: 'M', status: true },
+        ];
+        this.selectVelocidadbajada = [
+            { nombre: 'Kbps', value: 'K', status: true },
+            { nombre: 'Mbps', value: 'M', status: true },
+        ];
+    }
 
-  constructor(private _perfilfichas: PerfilfichasService) {
-    this.selectTipoFicha = [
-        {nombre: "Corrido", value: "CORRIDO", status: true},
-        {nombre: "Pausado", value: "PAUSADO", status: true},
-        {nombre: "Megas", value: "MEGAS", status: true},
-    ];
-    this.selectVelocidadsubida = [{nombre : "Kbps", value: "K", status: true}, {nombre : "Mbps", value: "M", status: true}]
-    this.selectVelocidadbajada = [{nombre : "Kbps", value: "K", status: true}, {nombre : "Mbps", value: "M", status: true}]
-  }
+    ngOnInit(): void {
+        this.obtenerplanesfichas();
+        /* this._perfilfichas.getPlanesFichas().subscribe(data => this.listplanesfichas = data); */
+    }
 
+    //Abre el modal de A
+    openNew() {
+        this.planes = {};
+        this.submitted = false;
+        this.perfilModalDialog = true;
+    }
 
-  ngOnInit(): void {
-    this.obtenerplanesfichas();
-    /* this._perfilfichas.getPlanesFichas().subscribe(data => this.listplanesfichas = data); */
-  }
+    obtenerplanesfichas() {
+        this._perfilfichas.getPlanesFichas().subscribe(
+            (data) => {
+                console.log(data);
+                this.listplanesfichas = data;
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
 
-//Abre el modal de A
-  openNew() {
-    this.planes = {};
-    this.submitted = false;
-    this.productDialog = true;
-}
+    agregarPlanesFichas(){
+        const planes : any = {
+            nameProfile : "DesdeFrontQWQWQ",
+            addressPoolProfile : "none",
+            velocidadSubidaBajadaProfile : "1M/3M",
+            limiteDeTiempoProfile : "5d",
+            onLoginProfile : "",
+            onLogoutProfile : "planesFichas.onLogoutProfil"
+        }
+    this._perfilfichas.savePlanesFichas(planes).subscribe(data =>{
+        this.obtenerplanesfichas();
+    }, error => {
+        console.log(error);
+    })
+    this.hideDialog();
+    }
 
-  obtenerplanesfichas(){
-        this._perfilfichas.getPlanesFichas().subscribe(data =>{
-            console.log(data)
-            this.listplanesfichas = data;
-        }, error => {
-            console.log(error);
-        })
-  }
+    editProduct(planes: any) {
+        console.log('el plan es' + planes);
+        this.planes = { ...planes };
+        this.perfilModalDialog = true;
+    }
 
-  editProduct(planes: any) {
-    console.log("el plan es" + planes);
-    this.planes = {...planes};
-    this.productDialog = true;
-}
-
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-}
-
-
-
+    hideDialog() {
+        this.perfilModalDialog = false;
+        this.submitted = false;
+    }
 }
--- src/app/demo/service/perfilfichas.service.ts
+++ src/app/demo/service/perfilfichas.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PerfilfichasComponent } from '../components/hotspot/perfilfichas/perfilfichas.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class PerfilfichasService {
 
     getPlanesFichas(): Observable<any>{
         return this.http.get(this.myAppURL + this.myApiUrl);
+    }
+
+    savePlanesFichas(planes: any): Observable<any>{
+        return this.http.post(this.myAppURL + this.myApiUrl, planes);
     }
 }
