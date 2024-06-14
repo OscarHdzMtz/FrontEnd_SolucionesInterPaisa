b4d8ffc971851e45de51871c83e130604a00b76f
OscarHernandez <oskr9922@gmail.com>
2024-06-14 16:30:13 -0600

    se agrego un funcion temporal en ngInInit de addhotspotuser para corregir error temporal de ngOninit

--- src/app/hotspot/components/hotspotusers/addhotspotuser/addhotspotuser.component.ts
+++ src/app/hotspot/components/hotspotusers/addhotspotuser/addhotspotuser.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-addhotspotuser',
-  templateUrl: './addhotspotuser.component.html',
-  styleUrls: ['./addhotspotuser.component.scss']
+  templateUrl: './addhotspotuser.component.html',  
 })
 export class AddhotspotuserComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    this.prueba();
+  }
+  prueba(){
+
   }
 
 }
